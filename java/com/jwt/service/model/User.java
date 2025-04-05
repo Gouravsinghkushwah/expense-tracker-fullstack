037ccf093e4263ed4274aaaba5386518b799645f
Gourav singh kushwah <71500489+Gouravsinghkushwah@users.noreply.github.com>
2025-04-06 00:14:43 +0530

    Update User.java

--- java/com/jwt/service/model/User.java
+++ java/com/jwt/service/model/User.java
@@ -1,12 +1,12 @@
-/*
- * package com.jwt.service.model;
- * 
- * import lombok.Data;
- * 
- * @Data public class User {
- * 
- * public User(String id, String name, String email) { this.id = id; this.name =
- * name; this.email = email;
- * 
- * } private String id; private String name; private String email; }
- */
+
+  package com.jwt.service.model;
+  
+  import lombok.Data;
+ 
+  @Data public class User {
+  
+  public User(String id, String name, String email) { this.id = id; this.name =
+  name; this.email = email;
+  
+  } private String id; private String name; private String email; }
+ 
