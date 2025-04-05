94e782d2dcc31f45da0a99767507b17a41d65bc1
Gourav singh kushwah <71500489+Gouravsinghkushwah@users.noreply.github.com>
2025-04-06 00:13:55 +0530

    Update JwtRequest.java

--- java/com/jwt/service/model/JwtRequest.java
+++ java/com/jwt/service/model/JwtRequest.java
@@ -1,11 +1,11 @@
-/*
- * package com.jwt.service.model;
- * 
- * import lombok.Data;
- * 
- * @Data public class JwtRequest {
- * 
- * private String email; private String password;
- * 
- * }
- */
+
+  package com.jwt.service.model;
+  
+  import lombok.Data;
+  
+  @Data public class JwtRequest {
+  
+  private String email; private String password;
+  
+  }
+ 
