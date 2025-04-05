2dc83cd86cce0230aa5884a50f5dce0c2039681b
Gourav singh kushwah <71500489+Gouravsinghkushwah@users.noreply.github.com>
2025-04-06 00:10:34 +0530

    Update HomeController.java

--- java/com/jwt/controller/HomeController.java
+++ java/com/jwt/controller/HomeController.java
@@ -1,32 +1,32 @@
-/*
- * package com.jwt.controller;
- * 
- * import java.security.Principal; import java.util.List;
- * 
- * import org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.web.bind.annotation.GetMapping; import
- * org.springframework.web.bind.annotation.RequestMapping; import
- * org.springframework.web.bind.annotation.RestController;
- * 
- * import com.jwt.service.USerService; import com.jwt.service.model.User;
- * 
- * @RestController
- * 
- * @RequestMapping("/home") public class HomeController {
- * 
- * @Autowired private USerService userService;
- * 
- * 
- * 
- * 
- * 
- * 
- * @GetMapping("/users") public List<User> getUser() {
- * System.out.println("Getting Users"); return this.userService.getUser(); }
- * 
- * @GetMapping("/current-user") public String getLoggedInUser(Principal
- * principal) { return principal.getName(); }
- * 
- * 
- * }
- */
+
+ package com.jwt.controller;
+  
+  import java.security.Principal; import java.util.List;
+  
+  import org.springframework.beans.factory.annotation.Autowired; import
+  org.springframework.web.bind.annotation.GetMapping; import
+  org.springframework.web.bind.annotation.RequestMapping; import
+  org.springframework.web.bind.annotation.RestController;
+  
+  import com.jwt.service.USerService; import com.jwt.service.model.User;
+  
+  @RestController
+  
+  @RequestMapping("/home") public class HomeController {
+  
+  @Autowired private USerService userService;
+  
+  
+  
+  
+  
+  
+  @GetMapping("/users") public List<User> getUser() {
+  System.out.println("Getting Users"); return this.userService.getUser(); }
+  
+  @GetMapping("/current-user") public String getLoggedInUser(Principal
+  principal) { return principal.getName(); }
+  
+  
+  }
+ 
