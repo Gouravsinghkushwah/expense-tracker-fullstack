153b6cad2adfc6af737a01578b0d49cb525ea2e4
Gourav singh kushwah <71500489+Gouravsinghkushwah@users.noreply.github.com>
2025-04-06 00:08:06 +0530

    Update AppConfig.java

--- java/com/jwt/config/AppConfig.java
+++ java/com/jwt/config/AppConfig.java
@@ -1,49 +1,43 @@
-/*
- * package com.jwt.config;
- * 
- * import java.beans.BeanProperty; import java.net.Authenticator;
- * 
- * import org.springframework.context.annotation.Bean; import
- * org.springframework.context.annotation.Configuration; import
- * org.springframework.security.authentication.AuthenticationManager; import
- * org.springframework.security.config.annotation.authentication.configuration.
- * AuthenticationConfiguration; import
- * org.springframework.security.core.userdetails.User; import
- * org.springframework.security.core.userdetails.UserDetails; import
- * org.springframework.security.core.userdetails.UserDetailsService; import
- * org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; import
- * org.springframework.security.crypto.password.PasswordEncoder; import
- * org.springframework.security.provisioning.InMemoryUserDetailsManager;
- * 
- * @Configuration public class AppConfig {
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * @Bean UserDetailsService detailsService() {
- * 
- * UserDetails userDetails =
- * User.builder().username("gourav").password(passwordEncoder().encode("gourav")
- * ).roles("ADMIN").build();
- * 
- * return new InMemoryUserDetailsManager(userDetails); }
- * 
- * @Bean PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder();
- * }
- * 
- * 
- * @Bean AuthenticationManager authenticationManager(AuthenticationConfiguration
- * builder) throws Exception { return builder.getAuthenticationManager(); }
- * 
- * 
- * 
- * }
- */
+  package com.jwt.config;
+  
+  import java.beans.BeanProperty; import java.net.Authenticator;
+  
+  import org.springframework.context.annotation.Bean; import
+  org.springframework.context.annotation.Configuration; import
+  org.springframework.security.authentication.AuthenticationManager; import
+  org.springframework.security.config.annotation.authentication.configuration.
+  AuthenticationConfiguration; import
+  org.springframework.security.core.userdetails.User; import
+  org.springframework.security.core.userdetails.UserDetails; import
+  org.springframework.security.core.userdetails.UserDetailsService; import
+  org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; import
+  org.springframework.security.crypto.password.PasswordEncoder; import
+  org.springframework.security.provisioning.InMemoryUserDetailsManager;
+  
+  @Configuration public class AppConfig {
+  
+
+
+
+
+
+
+   
+ @Bean UserDetailsService detailsService() {
+ 
+ UserDetails userDetails =
+  User.builder().username("gourav").password(passwordEncoder().encode("gourav")
+  ).roles("ADMIN").build();
+  
+  return new InMemoryUserDetailsManager(userDetails); }
+  
+  @Bean PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder();
+  }
+  
+  
+  @Bean AuthenticationManager authenticationManager(AuthenticationConfiguration
+  builder) throws Exception { return builder.getAuthenticationManager(); }
+  
+  
+  
+  }
