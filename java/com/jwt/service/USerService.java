d5f1fe1d4eaeec6f327587355c4ea32fae172e12
Gourav singh kushwah <71500489+Gouravsinghkushwah@users.noreply.github.com>
2025-04-06 00:15:12 +0530

    Update USerService.java

--- java/com/jwt/service/USerService.java
+++ java/com/jwt/service/USerService.java
@@ -1,29 +1,29 @@
-/*
- * package com.jwt.service;
- * 
- * import java.util.ArrayList; import java.util.List; import java.util.UUID;
- * 
- * import org.springframework.stereotype.Service;
- * 
- * import com.jwt.service.model.User;
- * 
- * @Service public class USerService {
- * 
- * private List<User> ul = new ArrayList<>();
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * public USerService() { ul.add(new
- * User(UUID.randomUUID().toString(),"Pawan","pawam@gmail.com")); ul.add(new
- * User(UUID.randomUUID().toString(),"Sumit","sumit@gmail.com")); ul.add(new
- * User(UUID.randomUUID().toString(),"kalu","kalu@gmail.com")); ul.add(new
- * User(UUID.randomUUID().toString(),"poonam","poonam@gmail.com")); }
- * 
- * 
- * public List<User> getUser() { return this.ul; } }
- */
+
+  package com.jwt.service;
+  
+  import java.util.ArrayList; import java.util.List; import java.util.UUID;
+  
+  import org.springframework.stereotype.Service;
+  
+  import com.jwt.service.model.User;
+  
+  @Service public class USerService {
+  
+  private List<User> ul = new ArrayList<>();
+  
+  
+  
+  
+  
+  
+  
+  
+  public USerService() { ul.add(new
+  User(UUID.randomUUID().toString(),"Pawan","pawam@gmail.com")); ul.add(new
+  User(UUID.randomUUID().toString(),"Sumit","sumit@gmail.com")); ul.add(new
+  User(UUID.randomUUID().toString(),"kalu","kalu@gmail.com")); ul.add(new
+  User(UUID.randomUUID().toString(),"poonam","poonam@gmail.com")); }
+  
+  
+  public List<User> getUser() { return this.ul; } }
+ 
